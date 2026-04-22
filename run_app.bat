6267c85bcf322d3b3ad9b1fa9c0a24e886a0b18c
%% <33155045+70T@users.noreply.github.com>
2026-04-22 15:22:32 +0300

    g

--- run_app.bat
+++ run_app.bat
@@ -1,4 +1,4 @@
 @echo off
 echo Running app.py...
-python app.py -f "C:\Users\Miro\Documents\Reading" --pdf-viewer "C:\Program Files\Okular\bin\okular.exe"
+python app.py
 pause
--- run_bot.bat
+++ run_bot.bat
@@ -1,4 +1,4 @@
 @echo off
 echo Running bot.py...
-python "C:\pdfsearch\bot.py"
+python bot.py
 pause
